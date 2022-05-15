24a4a434598d5c9bd079e2ac11626cccea4d5055
Kevin Rodrigues <63253812+KevinRodrigues23@users.noreply.github.com>
2022-05-14 21:19:34 -0300

    AtualizadoCadastroSenac

--- CadastroSenac/aluno.php
+++ CadastroSenac/aluno.php
@@ -1,15 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 
-
-
+    <link rel="stylesheet" href="css/main.css">
+    <title>Plataforma ||</title>
+</head>
+<body>
 <header class="menu">
-<link rel="stylesheet" href="css/main.css">
     <div class="headerDiv">
         <img class="divImg" src="img/1200px-Senac_logo.svg.png" >
     </div>
     <div class="headerDiv"></div>
     <div class="headerDiv">
-        <a href="aluno.php">
+        <a href="#">
         <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
             <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
@@ -24,3 +32,32 @@
          
     </div>
 </header>
+
+<div class="search">
+    <label class="col">Pesquisa Usuário</label>
+    <input class="form-control form-control-lg form-control-borderless" type="search" placeholder="Pesquisa usuário">
+</div>
+
+
+
+<main>
+    
+    <div class="mainDiv">
+       
+        <a href="index.php"><img class="mainIcon" src="img/search.png"></a>
+        <h2>BUSCAR</h2>
+    </div>
+ 
+    <div class="mainDiv">
+        <a href="#"><img class="mainIcon" src="img/edit.png"></a>
+        <h2>EDITAR</h2>
+        
+    </div>
+  
+
+</main>
+    <?php 
+    include("footer.php")
+    ?>
+</body>
+</html>
---
+++ CadastroSenac/login.php
@@ -0,0 +1,50 @@
+<?php 
+  include("header.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+<!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+    <title></title>
+</head>
+<body>
+
+    <div class="loginDiv">
+
+
+    <div class="formimg">
+        <img class="formImg" src="img/1200px-Senac_logo.svg.png" width="130px">
+        <h2>Login de Aluno</h2>
+        </div>
+        <hr>
+     
+
+    <form class="loginForm" action="" method="POST">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                <label for="exampleFormControlInput1" class="form-label">Senha</label>
+                <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="123456">
+                esqueceu a senha? <a href="#">clique aqui </a>
+            </div>
+
+            <div class="formButton"><button type="submit" class="btn btn-primary">Entrar</button></div>
+    </form>
+    </div> 
+
+    <!-- FOOTER -->
+    <?php 
+    include("footer.php")
+    ?>
+     
+</body>
+</html>
